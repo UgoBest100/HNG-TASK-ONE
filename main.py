52649efde2cb8153a97c61db5f6b4c18d612ea83
UgoBest <judelisent@gmail.com>
2025-01-30 19:15:45 +0100

    commit message

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ app.add_middleware(
 def read_root():
     return {
         "email": "judelisent@gmail.com",
-        "current_datetime": datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "current_datetime": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
         "github_url": "https://github.com/UgoBest100/HNG-TASK-ONE"
     }
